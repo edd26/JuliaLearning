b0c9c252d206bcd75a10fe57ba7b1bf519128d02
edd26 <ed18abc@herts.ac.uk>
2019-05-01 12:01:32 +0100

    Clean up of code

--- geometric_matrix_with_TDA.jl
+++ geometric_matrix_with_TDA.jl
@@ -16,13 +16,8 @@ using GraphPlot
 # [0, 1]d ⊂ Rd, for d ≤ N. The matrix entries decrease with distance, as
 # expected for geometrically organized correlations.
 
-# ### Unit cube of dimension "dimension" with the size of dimension "dims_size"
-
-# %%
-
-
 # Each column is a set of coordinates for a point,
-#   each row n is set of coordinates of all points in the n-th dimension
+#  each row n is set of coordinates of all points in the n-th dimension
 
 N = 88
 dimensions = 3
@@ -38,30 +33,29 @@ inverse_geometric_matrix = -geometric_matrix;
 
 # %%
 # # Odering Matrix
-#
 # Find the minimum value, return indicies, remove it, repeat until all are 0.0, inverse matrix.
 # First element in final "matrix ordering" is of points with smallest distance.
 # (The indexing is inversed in comparison to the article)
 
 elemnts_above_diagonal = Int((N^2-N)/2)
-matrix_ordering = zeros(Int, 2,elemnts_above_diagonal)
+    matrix_ordering = zeros(Int, 2,elemnts_above_diagonal)
 
-A = copy(geometric_matrix)
+    A = copy(geometric_matrix)
 
-for element in 1:elemnts_above_diagonal
-#     Find maximal distance
-    minimal_value = findmin(A)
-#     Get the coordinates (only 2 dimensions, because it is distance matrix)
-    matrix_ordering[1,element] = Int(minimal_value[2][1])
-    matrix_ordering[2,element] = Int(minimal_value[2][2])
-#
-# #     Zero minval in A (above and below diagonal) so next minval can be found
-    A[matrix_ordering[1,element], matrix_ordering[2,element]] = 0.0
-    A[matrix_ordering[2,element], matrix_ordering[1,element]] = 0.0
-end
+    for element in 1:elemnts_above_diagonal
+    #     Find maximal distance
+        minimal_value = findmin(A)
+    #     Get the coordinates (only 2 dimensions, because it is distance matrix)
+        matrix_ordering[1,element] = Int(minimal_value[2][1])
+        matrix_ordering[2,element] = Int(minimal_value[2][2])
+    #
+    # #     Zero minval in A (above and below diagonal) so next minval can be found
+        A[matrix_ordering[1,element], matrix_ordering[2,element]] = 0.0
+        A[matrix_ordering[2,element], matrix_ordering[1,element]] = 0.0
+    end
 
-# change from min to max order to the max to min order (? necessary ?)
-matrix_ordering = matrix_ordering[:,end:-1:1]
+    # change from min to max order to the max to min order (? necessary ?)
+    matrix_ordering = matrix_ordering[:,end:-1:1]
 
 
 # %% markdown
@@ -93,31 +87,34 @@ matrix_ordering = matrix_ordering[:,end:-1:1]
 
 # Edges are created between every points up to the level k
 edges = matrix_ordering
-num_of_edges = size(edges)[2]
+    num_of_edges = size(edges)[2]
 
-set_of_graphs = [a=Graph(vetrices) for a=1:num_of_edges]
-edges_counter = zeros(Int, num_of_edges)
-edge_density =  zeros(num_of_edges)
+    set_of_graphs = [a=Graph(vetrices) for a=1:num_of_edges]
+    edges_counter = zeros(Int, num_of_edges)
+    edge_density =  zeros(num_of_edges)
 
-k=1
-for k in range(1,stop=num_of_edges)~
-    add_edge!(set_of_graphs[k], edges[1,k], edges[2,k]);
-    edges_counter[k] = ne(set_of_graphs[k])
-    edge_density[k] = edges_counter[k]/binomial(N,2)
-    if k<num_of_edges
-        set_of_graphs[k+1] = copy(set_of_graphs[k])
+    k=1
+    for k in range(1,stop=num_of_edges)~
+        add_edge!(set_of_graphs[k], edges[1,k], edges[2,k]);
+        edges_counter[k] = ne(set_of_graphs[k])
+        edge_density[k] = edges_counter[k]/binomial(N,2)
+
+
+
+        if k<num_of_edges # if is used to eliminate copying at last iteration
+            set_of_graphs[k+1] = copy(set_of_graphs[k])
+        end
     end
-end
 
 # %%
 # Determine which graph shpuld be displayed
 n=300
 
-nodelabel = [r  for r in 1:nv(set_of_graphs[n])]
-println("Number of edges: ")
-println(edges_counter[n])
+    nodelabel = [r  for r in 1:nv(set_of_graphs[n])]
+    println("Number of edges: ")
+    println(edges_counter[n])
 
-println("Edge density: ")
-println(edge_density[n])
+    println("Edge density: ")
+    println(edge_density[n])
 
-gplot(set_of_graphs[n], layout=circular_layout, nodelabel=nodelabel)
+    gplot(set_of_graphs[n], layout=circular_layout, nodelabel=nodelabel)
