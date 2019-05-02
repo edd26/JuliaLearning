027c417ca464c1262e1b6fb5d7b50e8b30def950
edd26 <ed18abc@herts.ac.uk>
2019-05-02 17:15:34 +0100

    Minor changes in the printing text

--- clique_top_in_Julia.jl
+++ clique_top_in_Julia.jl
@@ -2,18 +2,6 @@ using MATLAB
  using DelimitedFiles
  using Plots
 
-
-geometric_matrix = readdlm( "geometric_matrix.csv",  ',', Float64, '\n')
- shuffeled_matrix = readdlm( "shuffeled_matrix.csv",  ',', Float64, '\n')
- random_matrix = readdlm( "random_matrix.csv",  ',', Float64, '\n')
-
-
-mat"ending = 60"
-mat"$geom_betti_numbers = compute_clique_topology($geometric_matrix(1:ending, 1:ending), 'Algorithm', 'split');"
-mat"$rand_betti_numbers = compute_clique_topology($random_matrix(1:ending, 1:ending), 'Algorithm', 'split');"
-mat"$shuf_betti_numbers = compute_clique_topology($shuffeled_matrix(1:ending, 1:ending), 'Algorithm', 'split');"
-
-
 function plot_betti_numbers(betti_numbers, title="Geometric  matrix")
     x_values = range(0,stop=0.6,length=size(betti_numbers)[1])
 
@@ -22,11 +10,28 @@ function plot_betti_numbers(betti_numbers, title="Geometric  matrix")
     plot!(x_values, betti_numbers[:,3], label="beta_2")
 end
 
-writedlm("geometric_betties.csv",  geom_betti_numbers, ',')
-writedlm("random_betties.csv",  rand_betti_numbers, ',')
-writedlm("shuffled_betties.csv",  shuf_betti_numbers, ',')
+
+geometric_matrix = readdlm( "geometric_matrix.csv",  ',', Float64, '\n')
+ shuffeled_matrix = readdlm( "shuffeled_matrix.csv",  ',', Float64, '\n')
+ random_matrix = readdlm( "random_matrix.csv",  ',', Float64, '\n')
+
+ending = 88
+
+println("Computing betti numbers for geometric matrix with $(ending)x$ending matrix.")
+ mat"$geom_betti_numbers = compute_clique_topology($geometric_matrix(1:$ending, 1:$ending), 'Algorithm', 'split');"
+
+println("Computing betti numbers for random matrix with $(ending)x$ending matrix.")
+ mat"$rand_betti_numbers = compute_clique_topology($random_matrix(1:$ending, 1:$ending), 'Algorithm', 'split');"
+
+println("Computing betti numbers for shuffled matrix with $(ending)x$ending matrix.")
+ mat"$shuf_betti_numbers = compute_clique_topology($shuffeled_matrix(1:$ending, 1:$ending), 'Algorithm', 'split');"
 
 
-plot_betti_numbers(geom_betti_numbers, "Geometric  matrix")
-plot_betti_numbers(rand_betti_numbers, "Random  matrix")
-plot_betti_numbers(shuf_betti_numbers, "Shuffled  matrix")
+writedlm("geometric_betties_$ending.csv",  geom_betti_numbers, ',')
+writedlm("random_betties_$ending.csv",  rand_betti_numbers, ',')
+writedlm("shuffled_betties_$ending.csv",  shuf_betti_numbers, ',')
+
+
+plot_betti_numbers(geom_betti_numbers, "Geometric  matrix, matrix size $ending")
+plot_betti_numbers(rand_betti_numbers, "Random  matrix, matrix size $ending")
+plot_betti_numbers(shuf_betti_numbers, "Shuffled  matrix, matrix size $ending")
