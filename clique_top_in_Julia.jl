645ae3b4d16bcc7bb5d72da7b1f2c7d9c0a3a02f
edd26 <ed18abc@herts.ac.uk>
2019-05-02 12:38:27 +0100

    Added plotting the betti numbers and saving them to the files

--- clique_top_in_Julia.jl
+++ clique_top_in_Julia.jl
@@ -1,5 +1,6 @@
 using MATLAB
-using DelimitedFiles
+ using DelimitedFiles
+ using Plots
 
 
 geometric_matrix = readdlm( "geometric_matrix.csv",  ',', Float64, '\n')
@@ -7,5 +8,25 @@ geometric_matrix = readdlm( "geometric_matrix.csv",  ',', Float64, '\n')
  random_matrix = readdlm( "random_matrix.csv",  ',', Float64, '\n')
 
 
-mat"ending = 50"
-mat"compute_clique_topology($geometric_matrix(1:ending, 1:ending), 'Algorithm', 'split');"
+mat"ending = 60"
+mat"$geom_betti_numbers = compute_clique_topology($geometric_matrix(1:ending, 1:ending), 'Algorithm', 'split');"
+mat"$rand_betti_numbers = compute_clique_topology($random_matrix(1:ending, 1:ending), 'Algorithm', 'split');"
+mat"$shuf_betti_numbers = compute_clique_topology($shuffeled_matrix(1:ending, 1:ending), 'Algorithm', 'split');"
+
+
+function plot_betti_numbers(betti_numbers, title="Geometric  matrix")
+    x_values = range(0,stop=0.6,length=size(betti_numbers)[1])
+
+    plot(x_values, betti_numbers[:,1], label="beta_0", title=title) #, ylims = (0,maxy)
+    plot!(x_values, betti_numbers[:,2], label="beta_1")
+    plot!(x_values, betti_numbers[:,3], label="beta_2")
+end
+
+writedlm("geometric_betties.csv",  geom_betti_numbers, ',')
+writedlm("random_betties.csv",  rand_betti_numbers, ',')
+writedlm("shuffled_betties.csv",  shuf_betti_numbers, ',')
+
+
+plot_betti_numbers(geom_betti_numbers, "Geometric  matrix")
+plot_betti_numbers(rand_betti_numbers, "Random  matrix")
+plot_betti_numbers(shuf_betti_numbers, "Shuffled  matrix")
