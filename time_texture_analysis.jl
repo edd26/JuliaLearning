44076ccf3e8374190c9030d2b33e1a550b4994fe
Emil Dmitruk <ed18abc@herts.ac.uk>
2019-05-13 12:46:48 +0100

    Completed code translation from MATLAB.

--- time_texture_analysis.jl
+++ time_texture_analysis.jl
@@ -1,20 +1,34 @@
 import Makie
-import VideoIO
+ import VideoIO
+ using MATLAB
+ using StatsBase
 
 ## Load video
 # video_file =  VideoReader('64caf10.avi');
 # video_file =  VideoReader('56ub310.avi');
-video_file = VideoIO.open("649j210.avi")
-video_info = video_file.video_info
+video_length = 0;
+ video_array = Vector{Array{UInt8}}(undef,0);
+ video_streamer = VideoIO.open("649j210.avi")
+ video_file = VideoIO.openvideo(video_streamer, target_format=VideoIO.AV_PIX_FMT_GRAY8)
+ vid_width = video_file.width;
+ vid_height = video_file.height;
 
-vid_width = video_file.Width;
-vid_height = video_file.Height;
-vid_frames_cont = video_file.Duration*video_file.FrameRate;
+
+# Read the video into the array
+
+while !eof(video_file)
+    push!(video_array,reinterpret(UInt8, read(video_file)))
+    global video_length += 1
+ end
+ close(video_file)
+
+# video_file.Duration*video_file.FrameRate;
 ## Create set of evenly distributed indicies
-number_of_points = 15;
-
-horizontal_indicies = 1:floor(vid_width/number_of_points):vid_width;
-vertical_indicies = 1:floor(vid_height/number_of_points):vid_height;
+number_of_points = 30;
+ horizontal_indicies = 1:Int64(floor(vid_width/number_of_points)):vid_width;
+ columns = size(horizontal_indicies,1)
+ vertical_indicies = 1:Int64(floor(vid_height/number_of_points)):vid_height;
+ rows = size(vertical_indicies,1)
 
 ## Create set of randomly distributed indicies
 # number_of_points = 20;
@@ -26,32 +40,22 @@ vertical_indicies = 1:floor(vid_height/number_of_points):vid_height;
 # vertical_indicies = uint64(vertical_indicies*vid_height);
 
 ## Extract  pixel changes
-extracted_pixels = zeros(size(vertical_indicies,2),...
-                         size(horizontal_indicies,2),...
-                         vid_frames_cont);
-
-frame_number = 1;
-while hasFrame(video_file)
-    vid_frame = readFrame(video_file);
-    gray_frame = rgb2gray(vid_frame);
-
-    extracted_pixels(:,:,frame_number) = gray_frame(vertical_indicies,...
-                                                    horizontal_indicies);
-    frame_number = frame_number + 1;
-end
+extracted_pixels = zeros(rows, columns, video_length);
+ for frame_number in 1:video_length
+    extracted_pixels[:,:,frame_number] = video_array[frame_number][vertical_indicies, horizontal_indicies]
+ end
 
 ## Reshape the extracted pixels to the vector form
-rows = size(extracted_pixels,1);
-number_of_signals = rows^2;
-vector_pixels = zeros(number_of_signals, length(extracted_pixels));
+number_of_signals = rows*columns
+ vectorized_video = zeros(number_of_signals, video_length);
 
 index = 1;
-for row=1:rows
-    for column=1:rows
-        vector_pixels(index,:) = extracted_pixels(row, column,:);
-        index = index+1;
+ for row=1:rows
+    for column=1:columns
+        vectorized_video[index,:] = extracted_pixels[row, column,:];
+        global index = index+1;
     end
-end
+ end
 
 # Consider the signals as neuron spike train in the total time duration T
 # At this point, the set of vectors may be considered as raw EEG recordings
@@ -64,15 +68,15 @@ end
 ## 1-3: Generate spike train from signal and
 # simulate the spike train with the same average fring rate
 threshold = 25;
-spike_interval = 25; #this works as a refraction period
-signal_duration = vid_frames_cont;
-spike_train = zeros(number_of_signals, signal_duration);
-simulated_spike_train = zeros(number_of_signals, signal_duration);
-average_firing_rate = zeros(number_of_signals, 1);
+ spike_interval = 25; #this works as a refraction period
+ signal_duration = vid_frames_cont;
+ spike_train = zeros(number_of_signals, video_length);
+ simulated_spike_train = zeros(number_of_signals, video_length);
+ average_firing_rate = zeros(number_of_signals, 1);
 
 for k = 1:number_of_signals
-#     [spike_count ,spike_index] = spike_times(vector_pixels(k,:),threshold);
-    [spike_count, spike_index] = get_spikes(vector_pixels(k,:),threshold, spike_interval);
+#     [spike_count ,spike_index] = spike_times(vectorized_video(k,:),threshold);
+    mat"[spike_count, spike_index] = get_spikes($vectorized_video($k,:),$threshold, $spike_interval);"
 
     spike_train(k,spike_index) = 1;
     average_firing_rate(k) = spike_count/signal_duration;
@@ -81,77 +85,31 @@ for k = 1:number_of_signals
 end
 
 ##
-C_ij = zeros(number_of_signals);
-T = size(vector_pixels,2);
-interval_length = 20;
-subs = zeros(T,1);
+C_ij = zeros(number_of_signals,number_of_signals);
+ T = video_length # size(vectorized_video,2);
+ interval_length = 20;
+ subs = zeros(T,1);
+ tau_max = 25; # this is given in frames
+ lags = -tau_max:1:tau_max
 
-# used_signal = vector_pixels;
-used_signal = spike_train;
+
+used_signal = vectorized_video;
+# used_signal = spike_train;
 
 for row=1:number_of_signals
     for column=1:number_of_signals
-        signal_ij = used_signal(row,:);
-        signal_ji = used_signal(column,:);
+        signal_ij = used_signal[row,:];
+        signal_ji = used_signal[column,:];
 
-        # Create mean over intervals
-        # This is taking the mean firing rate of the neuron spike train
-        subs = zeros(T,1);
-        for i=1:interval_length:T
-            subs(i:end) = subs(i:end) + 1;
-        end
-        sumed_signal_1 = accumarray(subs, signal_ij,[],@sum);
-        sumed_signal_2 = accumarray(subs, signal_ji,[],@sum);
-        time_axis = (1:length(sumed_signal_1))*interval_length ;
-# Plot
         # cross_corelation
-        [ccg_ij, lags] = xcorr(signal_ij, signal_ji); # , 'normalized'
-#       [ccg_ij, lags] = xcorr(sumed_signal_1, sumed_signal_2); # , 'normalized'
-#         [ccg_ji, lags] = xcorr(sumed_signal_2, sumed_signal_1);
-        ccg_ij = ccg_ij / T;
-#         ccg_ji = ccg_ji / T;
+        #  MATLAB is 10 times slower than Julia in computing crosscorrelation
+        ccg_ij = crosscov(signal_ij, signal_ji, lags);
+        ccg_ij = ccg_ij ./ T;
 
-        [ccg_rr, lags2] = xcorr(simulated_spike_train(row,:), ...
-                                simulated_spike_train(column,:));
-        r_i_r_j = mean(ccg_rr);
-        # correlation matrix
-        [max_val_ij, middle_ij] = max(ccg_ij);
-#         [max_val_ji, middle_ji] = max(ccg_ji);
-        middle_ij = floor(length(ccg_ij)/2);
-#         middle_ji = middle_ij;
-        tau_max = 25; # this is given in frames
-
-        A = sum(ccg_ij(middle_ij-tau_max:middle_ij+tau_max));
-#         B = sum(ccg_ji(middle_ji:middle_ji+tau_max ));
-        C_ij(row, column) = max([A])/(tau_max*r_i_r_j);
-
-# #  figure
-# clf('reset')
-# subplot(3, 1, 1)
-# hold on
-# plot(signal_ij)
-# plot(signal_ji)
-# hold off
-# xlabel("Frame number")
-# ylabel("Pixel value")
-# legend("Signal 1", "Signal 2")
-# title(sprintf("row: #d, column: #d",row, column))
-#
-# subplot(3, 1, 2)
-# hold on
-# plot(time_axis, sumed_signal_1, 'x-')
-# plot(time_axis, sumed_signal_2, 'x-')
-# hold off
-# xlabel("Frame number")
-#
-# subplot(3, 1, 3)
-# stem(lags, ccg_ij)
-# hold on
-# # plot(lags, ones(length(lags),1)*aver, lags, ones(length(lags),1)*C_ij(row, column))
-# hold off
-# xlabel("Frame bin shift")
-# ylabel("Normalized coerrelation")
-# legend("Crosscorelation", "r_i*r_j", "C_i_j")
+        A = sum(ccg_ij[tau_max+1:end]);
+        B = sum(ccg_ij[1:tau_max+1]);
+r_i_r_j = 1;
+        C_ij[row, column] = max(A, B)/(tau_max*r_i_r_j);
     end
 end
 
