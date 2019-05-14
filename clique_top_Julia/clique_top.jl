d9b760a9228ee8fc74f22e22b3f0151f170d5dce
Emil Dmitruk <ed18abc@herts.ac.uk>
2019-05-14 14:03:55 +0100

    Added directory verification

--- clique_top_Julia/clique_top.jl
+++ clique_top_Julia/clique_top.jl
@@ -92,243 +92,257 @@ function compute_clique_topology(inputMatrix;
                                     threads = 1)
 
 # # For testing only:
-# using MATLAB
-#  using DelimitedFiles
-#  using Plots
-#  using LinearAlgebra
-#
-# function plot_betti_numbers(betti_numbers, title="Geometric  matrix")
-#     x_values = range(0,stop=0.6,length=size(betti_numbers)[1])
-#
-#     plot(x_values, betti_numbers[:,1], label="beta_0", title=title) #, ylims = (0,maxy)
-#     plot!(x_values, betti_numbers[:,2], label="beta_1")
-#     plot!(x_values, betti_numbers[:,3], label="beta_2")
-#  end
-#
-#  function save_matrix_to_file(matrix, filename)
-#     open(filename, "w") do io
-#         writedlm(io,  matrix, ",")
-#     end
-#  end
-#
-#
-# geometric_matrix = readdlm( "geometric_matrix.csv",  ',', Float64, '\n')
-#
-# inputMatrix = geometric_matrix[1:20,1:20]
-#     reportProgress = false
-#     maxBettiNumber = 3
-#     edgeDensity = .6
-#     computeBetti0 = false
-#     filePrefix = "matrix"
-#     keepFiles = false
-#     workDirectory = "."
-#     writeMaxCliques = false
-#     algorithm = "split"
-#     threads = 1
+using MATLAB
+ using DelimitedFiles
+ using Plots
+ using LinearAlgebra
+ include("../GeometricMatrix.jl")
+
+
+geometric_matrix = readdlm( "geometric_matrix.csv",  ',', Float64, '\n')
+
+inputMatrix = geometric_matrix[1:40,1:40]
+    reportProgress = false
+    maxBettiNumber = 3
+    edgeDensity = .6
+    computeBetti0 = false
+    filePrefix = "matrix"
+    keepFiles = false
+    workDirectory = "."
+    writeMaxCliques = false
+    algorithm = "split"
+    threads = 1
 # # end testing
 
-    # ----------------------------------------------------------------
-    # Validate and set parameters
-    functionLocation = string(pwd(), "/compute_clique_topology.m")
-    baseDirectory = pwd()
+# ----------------------------------------------------------------
+# Validate and set parameters
+it_is = false
+do_not_change = false
 
-    perseusDirectory = string(baseDirectory, "/perseus")
-    neuralCodewareDirectory = string(baseDirectory, "/Neural_Codeware");
+current_location = pwd()
+folders = split(current_location, "/")
 
-    if (algorithm == "naive") || (algorithm=="parnaive") && writeMaxCliques
-        error("Naive clique enumeration and writeMaxCliques are incompatible");
+for element in folders
+    if element == "JuliaLearning"
+        println(element)
+        global it_is = true
     end
 
-    if (algorithm == "parnaive")
-        # empty in original file
+    if element == "clique_top_Julia"
+        global do_not_change = true
+    end
+end
+
+if !it_is
+    println("You are not in the right directory")
+    return
+end
+
+if !do_not_change
+    cd("clique_top_Julia/")
+    mat"cd('clique_top_Julia')"
+end
+
+baseDirectory = pwd()
+functionLocation = baseDirectory*"/compute_clique_topology.m"
+
+perseusDirectory = baseDirectory*"/perseus"
+neuralCodewareDirectory = baseDirectory*"/Neural_Codeware"
+
+if (algorithm == "naive") || (algorithm=="parnaive") && writeMaxCliques
+    error("Naive clique enumeration and writeMaxCliques are incompatible");
+end
+
+if (algorithm == "parnaive")
+    # empty in original file
+end
+
+# ----------------------------------------------------------------
+# If we need Cliquer, make sure the files are compiled
+if (algorithm == "split")
+    if isfile("./clique_top/Neural_Codeware/+Cliquer/FindAll.mexa64")
+        println("MEX Cliquer not compiled. Compiling before beginning process.")
+
+        startFolder = baseDirectory
+        # It needs to be checked what is tha execution dir of this funciton
+        cd("Neural_Codeware");
+        # cd("clique_top/Neural_Codeware");
+
+        # Compiler.compile()
+        # What compile does it compiles the c file and create mex file so that the c object can be used in matlab. The latter is not necessary
+
+        # The directory in which the Cliquer source code resides.
+        strDir = string(pwd(), "/+Cliquer", "/cliquer");
+
+        # clean old stuff
+        run(`make -C $strDir clean`);
+
+        # Build cliquer.
+        run(`make -C $strDir`);
+
+        # TODO Files are not moved anywhere
+
+        cd(startFolder);
+    end
+end
+
+# ----------------------------------------------------------------
+# Ensure that the diagonal is zero
+# ensure by zeroing the diagonal
+inputMatrix[Matrix{Bool}(I, size(inputMatrix))] .= 0
+
+# ----------------------------------------------------------------
+# Move to working directoy and stop if files might be overwritten
+mat"path($neuralCodewareDirectory, path);";
+try
+    cd(workDirectory);
+    if isfile(workDirectory * "/" * filePrefix * "_max_simplices.txt")
+        error("File $(filePrefix)_max_simplices.txt already exists in directory $(workDirectory).");
+    end
+    if isfile(workDirectory * "/" * filePrefix * "_simplices.txt")
+        error("File $(filePrefix)_simplices.txt already exists in directory $(workDirectory).");
+    end
+    if isfile(workDirectory * "/" * filePrefix * "_homology_betti.txt")
+        error("File $(filePrefix)_homology_betti.txt already exists in directory $(workDirectory).");
     end
 
-    # ----------------------------------------------------------------
-    # If we need Cliquer, make sure the files are compiled
-    if (algorithm == "split")
-        if isfile("./clique_top/Neural_Codeware/+Cliquer/FindAll.mexa64")
-            println("MEX Cliquer not compiled. Compiling before beginning process.")
+    for d=0:maxBettiNumber+1
+        if isfile(workDirectory * "/" * filePrefix * "_homology_" * string(d) *  ".txt")
+            error("File ($filePrefix)_homology_($d).txt already exists in directory ($workDirectory).");
+        end
+    end
+catch exception
+    # disp(exception.message);
+    rethrow(exception);
+end
 
-            startFolder = baseDirectory
-            # It needs to be checked what is tha execution dir of this funciton
-            cd("Neural_Codeware");
-            # cd("clique_top/Neural_Codeware");
+# ----------------------------------------------------------------
+# Enumerate maximal cliques and print to Perseus input file
 
-            # Compiler.compile()
-            # What compile does it compiles the c file and create mex file so that the c object can be used in matlab. The latter is not necessary
+if reportProgress
+    # toc;
+    println("Enumerating cliques using $algorithm algorithm.");
+    # tic;
+end
 
-            # The directory in which the Cliquer source code resides.
-            strDir = string(pwd(), "/+Cliquer", "/cliquer");
+if algorithm == "combine"
+        mat"numFiltrations = combine_cliques_and_write_to_file(...
+            inputMatrix, maxBettiNumber + 2, maxEdgeDensity, filePrefix,...
+            writeMaxCliques);"
+elseif algorithm=="naive"
+        mat"numFiltrations = naive_enumerate_cliques_and_write_to_file(...
+            inputMatrix, maxBettiNumber + 2, maxEdgeDensity, filePrefix);"
+elseif algorithm=="parnaive"
+        mat"numFiltrations = ...
+            parallel_naive_enumerate_cliques_and_write_to_file(...
+            inputMatrix, maxBettiNumber + 2, maxEdgeDensity, filePrefix,...
+            numThreads );"
+elseif algorithm=="split"
+        numFiltrations =
+            mat"pwd
+            split_cliques_and_write_to_file($inputMatrix, $maxBettiNumber + 2, $edgeDensity, $filePrefix,$writeMaxCliques);"
+end
 
-            # clean old stuff
-            run(`make -C $strDir clean`);
+# ----------------------------------------------------------------
+# Use Perseus to compute persistent homology
 
-            # Build cliquer.
-            run(`make -C $strDir`);
+if reportProgress
+    # toc;
+    println("Using Perseus to compute persistent homology.");
+    # tic;
+end
 
-            # TODO Files are not moved anywhere
+# run_perseus(filePrefix, perseusDirectory);
+if Sys.isapple()
+    perseusCommand = "perseusMac"
+elseif Sys.iswindows()
+    perseusCommand = "perseusWin"
+elseif Sys.islinux()
+    perseusCommand = "perseusLin"
+else
+    error("Cannot determine operating system type to run Perseus.");
+end
 
-            cd(startFolder);
+run(`$perseusDirectory/$perseusCommand nmfsimtop $(filePrefix)_simplices.txt $(filePrefix)_homology`)
+
+if reportProgress
+    # toc;
+end
+
+# ----------------------------------------------------------------
+# Assemble the results of the computation for output
+matrixSize = size(inputMatrix, 1)
+edgeDensities = (1:numFiltrations) / binomial(matrixSize,2)
+
+try
+    bettiCurves = mat"read_perseus_bettis(sprintf('%s_homology_betti.txt',...
+        $filePrefix), $numFiltrations, $maxBettiNumber, $computeBetti0);"
+
+    if computeBetti0
+        persistenceIntervals = zeros(Int64(numFiltrations), maxBettiNumber+1);
+        unboundedIntervals = zeros(1,maxBettiNumber+1);
+
+        for d=0:maxBettiNumber
+            persistenceIntervals[:,d+1], unboundedIntervals[d+1] = mat" read_persistence_interval_distribution(...
+            sprintf('%s_homology_%i.txt', $filePrefix, $d), ...
+            $numFiltrations)"
+        end
+    else
+        persistenceIntervals = zeros(Int64(numFiltrations), maxBettiNumber);
+        unboundedIntervals = zeros(1,maxBettiNumber);
+
+        for d=1:Int64(maxBettiNumber)
+            # A[:,d], B = mat" read_persistence_interval_distribution(...
+            # sprintf('%s_homology_%i.txt', $filePrefix, $d), ...
+            # $numFiltrations)"
+            # persistenceIntervals[:,d] = A
+            # unboundedIntervals[d] = B
+            mat"[$persistenceIntervals(:,$d), $unboundedIntervals($d) ] =...
+                    read_persistence_interval_distribution(...
+                    sprintf('%s_homology_%i.txt', $filePrefix, $d), ...
+                    $numFiltrations);"
         end
     end
 
-    # ----------------------------------------------------------------
-    # Ensure that the diagonal is zero
-    # ensure by zeroing the diagonal
-    inputMatrix[Matrix{Bool}(I, size(inputMatrix))] .= 0
+catch exception
+    # println(exception.message);
+    println("Failure to read Perseus output files. This error has likely occurred due to the Perseus process aborting due to memory limitations. It may be possible to circumvent this difficulty by reducing either the maximum Betti number or the maximum edge density computed. Please see the CliqueTop documentation for details.");
+    rethrow(exception);
+end
 
-    # ----------------------------------------------------------------
-    # Move to working directoy and stop if files might be overwritten
-    mat"path($neuralCodewareDirectory, path);";
+
+# ----------------------------------------------------------------
+# Remove remaining intermediate files if desired
+
+if keepFiles == false
     try
-        cd(workDirectory);
-        if isfile(string(workDirectory, "/", filePrefix, "_max_simplices.txt"))
-            error("File $(filePrefix)_max_simplices.txt already exists in directory $(workDirectory).");
+        max_simp_path = string(workDirectory, "/", filePrefix, "_max_simplices.txt")
+        if isfile(max_simp_path)
+            run(`rm $max_simp_path`)
         end
-        if isfile(string(workDirectory, "/", filePrefix, "_simplices.txt"))
-            error("File $(filePrefix)_simplices.txt already exists in directory $(workDirectory).");
+
+        simp_path = string(workDirectory, "/", filePrefix, "_simplices.txt")
+        if isfile(simp_path)
+            run(`rm $simp_path`)
         end
-        if isfile(string(workDirectory, "/", filePrefix, "_homology_betti.txt"))
-            error("File $(filePrefix)_homology_betti.txt already exists in directory $(workDirectory).");
+
+        hom_betti_path = string(workDirectory, "/", filePrefix, "_max_simplices.txt")
+        if isfile(hom_betti_path)
+            run(`rm $hom_betti_path`)
         end
 
         for d=0:maxBettiNumber+1
-            if isfile(string(workDirectory, "/", filePrefix, "_homology_", string(d), ".txt"))
-                error("File ($filePrefix)_homology_($d).txt already exists in directory ($workDirectory).");
+            max_betti_path = string(workDirectory, "/", filePrefix, "_homology_", string(d), ".txt")
+            if isfile(max_betti_path)
+                run(`rm $max_betti_path`)
             end
         end
     catch exception
-        # disp(exception.message);
+        println(exception.message);
         rethrow(exception);
     end
-
-    # ----------------------------------------------------------------
-    # Enumerate maximal cliques and print to Perseus input file
-
-    if reportProgress
-        # toc;
-        println("Enumerating cliques using $algorithm algorithm.");
-        # tic;
-    end
-
-    if algorithm == "combine"
-            mat"numFiltrations = combine_cliques_and_write_to_file(...
-                inputMatrix, maxBettiNumber + 2, maxEdgeDensity, filePrefix,...
-                writeMaxCliques);"
-    elseif algorithm=="naive"
-            mat"numFiltrations = naive_enumerate_cliques_and_write_to_file(...
-                inputMatrix, maxBettiNumber + 2, maxEdgeDensity, filePrefix);"
-    elseif algorithm=="parnaive"
-            mat"numFiltrations = ...
-                parallel_naive_enumerate_cliques_and_write_to_file(...
-                inputMatrix, maxBettiNumber + 2, maxEdgeDensity, filePrefix,...
-                numThreads );"
-    elseif algorithm=="split"
-            numFiltrations = mat"split_cliques_and_write_to_file(...
-                $inputMatrix, $maxBettiNumber + 2, $edgeDensity, $filePrefix,...
-                $writeMaxCliques);"
-    end
-
-    # ----------------------------------------------------------------
-    # Use Perseus to compute persistent homology
-
-    if reportProgress
-        # toc;
-        println("Using Perseus to compute persistent homology.");
-        # tic;
-    end
-
-    # run_perseus(filePrefix, perseusDirectory);
-    if Sys.isapple()
-        perseusCommand = "perseusMac"
-    elseif Sys.iswindows()
-        perseusCommand = "perseusWin"
-    elseif Sys.islinux()
-        perseusCommand = "perseusLin"
-    else
-        error("Cannot determine operating system type to run Perseus.");
-    end
-
-    run(`$perseusDirectory/$perseusCommand nmfsimtop $(filePrefix)_simplices.txt $(filePrefix)_homology`)
-
-    if reportProgress
-        # toc;
-    end
-
-    # ----------------------------------------------------------------
-    # Assemble the results of the computation for output
-    matrixSize = size(inputMatrix, 1)
-    edgeDensities = (1:numFiltrations) / binomial(matrixSize,2)
-
-    try
-        bettiCurves = mat"read_perseus_bettis(sprintf('%s_homology_betti.txt',...
-            $filePrefix), $numFiltrations, $maxBettiNumber, $computeBetti0);"
-
-        if computeBetti0
-            persistenceIntervals = zeros(Int64(numFiltrations), maxBettiNumber+1);
-            unboundedIntervals = zeros(1,maxBettiNumber+1);
-
-            for d=0:maxBettiNumber
-                persistenceIntervals[:,d+1], unboundedIntervals[d+1] = mat" read_persistence_interval_distribution(...
-                sprintf('%s_homology_%i.txt', $filePrefix, $d), ...
-                $numFiltrations)"
-            end
-        else
-            persistenceIntervals = zeros(Int64(numFiltrations), maxBettiNumber);
-            unboundedIntervals = zeros(1,maxBettiNumber);
-
-            for d=1:Int64(maxBettiNumber)
-                # A[:,d], B = mat" read_persistence_interval_distribution(...
-                # sprintf('%s_homology_%i.txt', $filePrefix, $d), ...
-                # $numFiltrations)"
-                # persistenceIntervals[:,d] = A
-                # unboundedIntervals[d] = B
-                mat"[$persistenceIntervals(:,$d), $unboundedIntervals($d) ] =...
-                        read_persistence_interval_distribution(...
-                        sprintf('%s_homology_%i.txt', $filePrefix, $d), ...
-                        $numFiltrations);"
-            end
-        end
-
-    catch exception
-        # println(exception.message);
-        println("Failure to read Perseus output files. This error has likely occurred due to the Perseus process aborting due to memory limitations. It may be possible to circumvent this difficulty by reducing either the maximum Betti number or the maximum edge density computed. Please see the CliqueTop documentation for details.");
-        rethrow(exception);
-    end
+end
 
 
-    # ----------------------------------------------------------------
-    # Remove remaining intermediate files if desired
-
-    if keepFiles == false
-        try
-            max_simp_path = string(workDirectory, "/", filePrefix, "_max_simplices.txt")
-            if isfile(max_simp_path)
-                run(`rm $max_simp_path`)
-            end
-
-            simp_path = string(workDirectory, "/", filePrefix, "_simplices.txt")
-            if isfile(simp_path)
-                run(`rm $simp_path`)
-            end
-
-            hom_betti_path = string(workDirectory, "/", filePrefix, "_max_simplices.txt")
-            if isfile(hom_betti_path)
-                run(`rm $hom_betti_path`)
-            end
-
-            for d=0:maxBettiNumber+1
-                max_betti_path = string(workDirectory, "/", filePrefix, "_homology_", string(d), ".txt")
-                if isfile(max_betti_path)
-                    run(`rm $max_betti_path`)
-                end
-            end
-        catch exception
-            println(exception.message);
-            rethrow(exception);
-        end
-    end
-
-
-    return bettiCurves, edgeDensities, persistenceIntervals, unboundedIntervals
+return bettiCurves, edgeDensities, persistenceIntervals, unboundedIntervals
 end
