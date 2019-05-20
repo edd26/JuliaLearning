65feb97ddd2baf2c0772b15dfe6a53e5f6ca6915
Emil Dmitruk <ed18abc@herts.ac.uk>
2019-05-20 14:00:54 +0100

    Adde function for loading the gif into array

--- GifGenerator.jl
+++ GifGenerator.jl
@@ -1,4 +1,21 @@
-using Luxor, Colors
+using Luxor, Colors, FileIO
+
+"""
+Generate a gif at @full_path using the function @frame_function. Image size and
+ length sotred in the tuple @gif_dims.
+"""
+function generate_gif(frame_function, full_path, gif_dims)
+    w, h, len = gif_dims
+    len -= 1
+    demo = Movie(w,h, "test")
+
+    animate(demo, [
+        Scene(demo, backdrop, 0:len),
+        Scene(demo, frame_function, 0:len, easingfunction=easeinoutcubic)
+                    ], creategif=true, pathname=full_path,  usenewffmpeg=false)
+    @info "Generation finished"
+end
+
 
 """
 Generate a frame filled whith dots moving toward or out form the middle.
@@ -6,14 +23,14 @@ Generate a frame filled whith dots moving toward or out form the middle.
 function dotted_plane(scene, framenumber)
     distance = mod(framenumber,50)
     if distance <= 25
-        for radius = distance:5:300
+        for radius = distance:5:800
             setdash("dot")
             sethue("gray30")
             A, B = [Point(x, 0) for x in [-radius, radius]]
             circle(O, radius, :stroke)
         end
     else
-        for radius = (50-distance):5:300
+        for radius = (50-distance):5:800
             setdash("dot")
             sethue("gray30")
             A, B = [Point(x, 0) for x in [-radius, radius]]
@@ -28,4 +45,16 @@ Function for background.
 """
 function backdrop(scene, framenumber)
     background("white")
+end
+
+
+"""
+Load the gif to the array.
+
+The load function is he FileIO function, but its possibilities are enchanced by
+    the ImageMagic package. ImageMagic should not be loaded to project- it is
+    handled by FileIO.
+"""
+function load_gif_to_array(fullpath)
+    return img = load(path*gif_name)
 end
