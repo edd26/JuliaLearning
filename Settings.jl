b4b1b49cfddaaf45e61013c573228009ff750143
Emil Dmitruk <ed18abc@herts.ac.uk>
2019-05-21 09:29:47 +0100

    Added settings file as storage for all paths and constant variables

---
+++ Settings.jl
@@ -0,0 +1,10 @@
+using Plots
+
+include("VideoManage.jl")
+include("MatrixToolbox.jl")
+include("clique_top_Julia/CliqueTop.jl")
+
+
+
+video_path = "/home/ed19aaf/Programming/Julia/JuliaLearning/videos/"
+results_path = "/home/ed19aaf/Programming/Julia/JuliaLearning/results_patch_average/"
