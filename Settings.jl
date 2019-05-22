4e541663ec0760348f153226cd3a64a77300a378
Emil Dmitruk <ed18abc@herts.ac.uk>
2019-05-22 17:33:14 +0100

    Added VIDEO variables for textures database

--- Settings.jl
+++ Settings.jl
@@ -1,7 +1,7 @@
 using Plots
 using Dates
 
-include("TestingPairwiseCorrelationmatrix.jl.jl")
+# include("TestingPairwiseCorrelationmatrix.jl")
 
 
 VIDEO = (diag_1=1,
@@ -10,7 +10,11 @@ VIDEO = (diag_1=1,
             diag_g2=4,
             diag_gb=5,
             diag_dbl=6,
-            horiz=7)
+            horiz=7,
+            water=8,
+            reef=9,
+            candle=10,
+            checkboard=11)
 
 DISTRIBUTION = (uniform="uniform",
                 random="random",
@@ -58,7 +62,7 @@ test_params["video_name"] = videos_names[video_choice]
 test_params["ind_distrib"] = DISTRIBUTION.uniform
 test_params["shift_set"] = [2 4]
 
-#TODO saving and loading from multiple files
+#TODO saving and loading from multiple files with JSON package
 
 # videos_set = [video_choice]
 # tau_max_set = [test_params["tau_max"]]
