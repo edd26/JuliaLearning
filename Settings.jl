5d77a948a1759bfa9cddfa024c130c269bcae50f
Emil Dmitruk <ed18abc@herts.ac.uk>
2019-05-22 11:46:43 +0100

    Renamed testing_parameters to test_param and added variables for patch mask

--- Settings.jl
+++ Settings.jl
@@ -13,17 +13,11 @@ VIDEO = (diag_1=1,
             diag_dbl=6,
             horiz=7)
 
+DISTRIBUTION = (uniform="uniform",
+                random="random",
+                patch="patch")
+
 video_choice = VIDEO.diag_1
-
-# videos_names = ["diag_strip_30sec_1.mov",
-#                 "diag_strip_30sec_2.mov",
-#                 "diag_strip_30sec_gray_1.mov",
-#                 "diag_strip_30sec_gray_2.mov",
-#                 "diag_strip_30sec_gray_both.mov",
-#                 "diag_strip_30sec_single_dbl_gaps.mov",
-#                 "horiz_strip_30sec.mov"]
-
-
 
 video_path = "/home/ed19aaf/Programming/Julia/JuliaLearning/videos/"
 results_path = "/home/ed19aaf/Programming/Julia/JuliaLearning/results/"
@@ -49,27 +43,27 @@ cd(current_path)
 videos_names = readdir(video_path)
 
 # ---------------
-testing_paramenters = Dict()
-testing_paramenters["do_clique_top"] = true
-testing_paramenters["do_eirene"] = false
-# testing_paramenters["choice"] = VIDEO.diag_gb
-testing_paramenters["save_figures"] = true
-testing_paramenters["plot_betti_figrues"] = true
-testing_paramenters["plot_vectorized_video"] = true
-testing_paramenters["tau_max"] = 25
-testing_paramenters["points_per_dim"] = 9
-testing_paramenters["size_limiter"] = 20
-testing_paramenters["use_testing_set"] = true
-testing_paramenters["video_name"] = videos_names[video_choice]
+test_params = Dict()
+test_params["do_clique_top"] = true
+test_params["do_eirene"] = false
+# test_params["choice"] = VIDEO.diag_gb
+test_params["save_figures"] = true
+test_params["plot_betti_figrues"] = true
+test_params["plot_vectorized_video"] = true
+test_params["tau_max"] = 25
+test_params["points_per_dim"] = 9
+test_params["size_limiter"] = 20
+test_params["use_testing_set"] = true
+test_params["video_name"] = videos_names[video_choice]
+test_params["indicies_distribution"] = DISTRIBUTION.uniform
+
+#TODO saving and loading from file
 
 # videos_set = [video_choice]
-# tau_max_set = [testing_paramenters["tau_max"]]
-# points_per_dim_set = [testing_paramenters["points_per_dim"]]
+# tau_max_set = [test_params["tau_max"]]
+# points_per_dim_set = [test_params["points_per_dim"]]
 
 videos_set = [8 10]
 tau_max_set = [5 10 20 25]
 points_per_dim_set = [4 9 12]
-
-# for video in videos_set
-#     println(video)
-# end
+focus_center = Dict("x"=>1, "y"=>1)
