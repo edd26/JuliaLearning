fe7ec73b3c083894a45aa5e62f31cc87de8ce62e
Emil Dmitruk <edmitruk@mion.elka.pw.edu.pl>
2019-05-27 15:18:01 +0100

    Added description of the funcitons.

--- MatrixToolbox.jl
+++ MatrixToolbox.jl
@@ -1,24 +1,35 @@
 using Distances
- using DataFrames
- using Random
- using LightGraphs
- using GraphPlot
+using DataFrames
+using Random
+using LightGraphs
+using GraphPlot
 
+"""
+Returns a random matrix of size @number_of_points x @dimensions in which every
+column is a point and every n-th row is a position in the n-th dimension.
+"""
 generate_random_point_cloud(number_of_points = 12, dimensions=2) =
                                     rand(Float64, dimensions, number_of_points)
 
 
+"""
+Return a matrix which stores the pariwise distances between every point in the
+@random_points matrix.
+"""
 function generate_geometric_matrix(random_points)
     geometric_matrix = pairwise(Euclidean(), random_points, dims=1)
     return geometric_matrix
 end
 
-function generate_shuffled_matrix(geometric_matrix)
-    matrix_size = size(geometric_matrix,1)
+"""
+Returns a symetric matrix with randomly permuted valuse from the @input_matrix.
+"""
+function generate_shuffled_matrix(input_matrix)
+    matrix_size = size(input_matrix,1)
 
-    indicies_collection = findall(x->x>0, geometric_matrix)
+    indicies_collection = findall(x->x>0, input_matrix)
     rand!(indicies_collection, indicies_collection)
-    shuffeled_matrix = copy(geometric_matrix)
+    shuffeled_matrix = copy(input_matrix)
 
     # Swap the elements
     n=1
@@ -26,11 +37,11 @@ function generate_shuffled_matrix(geometric_matrix)
         for m in k+1:matrix_size
             a = indicies_collection[n][1]
             b = indicies_collection[n][2]
-            shuffeled_matrix[k,m] = geometric_matrix[a,b]
-            shuffeled_matrix[m,k] = geometric_matrix[b,a]
+            shuffeled_matrix[k,m] = input_matrix[a,b]
+            shuffeled_matrix[m,k] = input_matrix[b,a]
 
-            shuffeled_matrix[a,b] = geometric_matrix[k,m]
-            shuffeled_matrix[b,a] = geometric_matrix[m,k]
+            shuffeled_matrix[a,b] = input_matrix[k,m]
+            shuffeled_matrix[b,a] = input_matrix[m,k]
 
             n +=1
         end
@@ -38,6 +49,10 @@ function generate_shuffled_matrix(geometric_matrix)
     return shuffeled_matrix
 end
 
+"""
+Returns matrix with random values which are symmetric accros the diagonal. The
+matrix has @matrix_size rows and @matrix_size columns.
+"""
 function generate_random_matrix(matrix_size)
     elemnts_above_diagonal = Int((matrix_size^2-matrix_size)/2)
     random_matrix = zeros(matrix_size, matrix_size)
@@ -55,7 +70,12 @@ function generate_random_matrix(matrix_size)
     return random_matrix
 end
 
-function generate_matrix_ordering(geometric_matrix)
+"""
+Returns ordering of the @geometric_matrix given as an input. If value @ascending
+is set to true, the values are number from the lowest value, to the highest. If
+false, the values are numbered from highest to the lowest.
+"""
+function generate_matrix_ordering(geometric_matrix, ascending = true)
     matrix_size = size(geometric_matrix, 2)
     elemnts_above_diagonal = Int((matrix_size^2-matrix_size)/2)
     matrix_ordering = zeros(Int, 2,elemnts_above_diagonal)
@@ -64,7 +84,7 @@ function generate_matrix_ordering(geometric_matrix)
 
     for element in 1:elemnts_above_diagonal
     #     Find maximal distance
-        minimal_value = findmin(A)
+        minimal_value = method(A)
     #     Get the coordinates (only 2 dimensions, because it is distance matrix)
         matrix_ordering[1,element] = Int(minimal_value[2][1])
         matrix_ordering[2,element] = Int(minimal_value[2][2])
@@ -75,11 +95,19 @@ function generate_matrix_ordering(geometric_matrix)
     end
 
     # change from min to max order to the max to min order (? necessary ?)
-    matrix_ordering = matrix_ordering[:,end:-1:1]
+    if ascending
+        matrix_ordering = matrix_ordering[:,end:-1:1]
+    end
 
     return matrix_ordering
 end
 
+"""
+Returns set of graphs generated from the @matrix_ordering. In every succesive
+graph, single connection between points is added.
+
+NOTE: the function does not take the coordinates of the numbered vertices.
+"""
 function generate_set_of_graphs(matrix_size, matrix_ordering)
     vetrices = matrix_size
     edges = matrix_ordering
@@ -102,6 +130,10 @@ function generate_set_of_graphs(matrix_size, matrix_ordering)
     return set_of_graphs, edge_density
 end
 
+"""
+Plots Betti curves. The betti numbers should be obtained with the clique-top
+library.
+"""
 function plot_betti_numbers(betti_numbers, edge_density, title="Geometric  matrix"; stop=0.6)
     p1 = plot(edge_density, betti_numbers[:,1], label="beta_0", title=title, legend=:topleft) #, ylims = (0,maxy)
     plot!(edge_density, betti_numbers[:,2], label="beta_1")
@@ -112,6 +144,10 @@ function plot_betti_numbers(betti_numbers, edge_density, title="Geometric  matri
     return p1
 end
 
+"""
+Saves given @matrix to the csv file with the name @filename. If there is no path 
+added to the @filename, then file saved is in local folder.
+"""
 function save_matrix_to_file(matrix, filename)
     open(filename, "w") do io
         writedlm(io,  matrix, ',')
