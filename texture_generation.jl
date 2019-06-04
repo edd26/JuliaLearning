7c1b3c5fb4ce9ecd160f62bc54bfe2c7ba344e9e
Emil Dmitruk <ed18abc@herts.ac.uk>
2019-06-04 13:34:34 +0100

    Minor code rearrangements

--- texture_generation.jl
+++ texture_generation.jl
@@ -14,21 +14,23 @@ mat"cd('textureSynth')"
 # s1 = MSession()
 
 # Load the image to replicate:
-choice = test_params["videos_names"][2]
-video_array = get_video_array_from_file(test_params["video_path"]*choice)
-video_dimensions = get_video_dimension(video_array)
-full_img = video_array[1]
+k=11
+ choice = test_params["videos_names"][k]
+ video_array = get_video_array_from_file(test_params["video_path"]*choice)
+ video_dimensions = get_video_dimension(video_array)
+ full_img = video_array[1]
 
-im = convert.(Int64, full_img)
-save("textureSynth/test_img.png", colorview(Gray, normalize_to_01(im0)))
-@mput im
+ im = convert.(Int64, full_img)
+ # save("/home/ed19aaf/Dropbox/P_S_images/img$(k)_50.png", colorview(Gray, normalize_to_01(im)))
+ @mput im
 
 mat"
 	close all
 
 	% im0 is a double float matrix!
+    im0 = pgmRead('text.pgm');
 	% im0 = double(imread('test_img.png'));
-	im0 = double(im);
+	% im0 = double(im);
 
 
 	Nsc = 4; % Number of scales
@@ -38,7 +40,7 @@ mat"
 
 	params = textureAnalysis(im0, Nsc, Nor, Na);
 
-	Niter = 50;	% Number of iterations of synthesis loop
+	Niter = 25;	% Number of iterations of synthesis loop
 	Nsx = 256;	% Size of synthetic image is Nsy x Nsx
 	Nsy = 256;	% WARNING: Both dimensions must be multiple of 2^(Nsc+2)
 
@@ -48,6 +50,8 @@ mat"
 	figure(1)
 	showIm(im0, 'auto', 1, 'Original texture');
 	figure(2)
-	showIm(res, 'auto', 1, 'Synthesized texture');"
+	showIm(res, 'auto', 1, 'Synthesized texture');
 
-mat"pwd()"
+    %imwrite(res, 'img$(k)_50-256-256_synthesis.png')
+    "
+# mat"pwd()"
