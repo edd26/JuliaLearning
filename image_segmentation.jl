7c26d7e28bb61b29fd9b328a6cbe2235e7268d65
Emil Dmitruk <ed18abc@herts.ac.uk>
2019-06-21 15:24:33 +0100

    Added computation of the centers of the segments

--- image_segmentation.jl
+++ image_segmentation.jl
@@ -4,34 +4,43 @@ using Random
 using FFTW, Plots
 
 using Clustering
+using Statistics
+
 
 img = Gray.(load("img/checkerboard.png"))
 imshow(img)
 
-imgg = imfilter(img, Kernel.gaussian(3));
-segments = unseeded_region_growing(imgg, 0.08)
-    imshow(map(i->segment_mean(segments,i), labels_map(segments)));
+imgg = imfilter(img, Kernel.gaussian(1));
+imshow(imgg)
 
-segments = unseeded_region_growing(img, 0.08)
-    imshow(map(i->segment_mean(segments,i), labels_map(segments)));
+# segments = unseeded_region_growing(imgg, 0.08)
+#     imshow(map(i->segment_mean(segments,i), labels_map(segments)));
 
-# Method below takes a lot of time time
+
+# Method below takes a lot of time time but returns multiple segments
 segments = meanshift(imgg, 16, 10/255)
  # parameters are smoothing radii: spatial=16, intensity-wise=8/255
     imshow(map(i->segment_mean(segments,i), labels_map(segments)))
 #
 
 # Get the center of the segments
+seg_pix_count = segment_pixel_count(segments)
+seg_map = labels_map(segments)
+size_threshold = 21
+seg_centers = Dict()
 
-findmin(labels_map(segments))
+for segment in segment_labels(segments)
+   if seg_pix_count[segment] > size_threshold
+      index_set = findall(x->x==segment, seg_map)
+      indicies = size(index_set)[1]
+      center_x = 0
+      center_y = 0
+      for k = 1:indicies
+         center_x += index_set[k][1]
+         center_y += index_set[k][2]
+      end
+      seg_centers[segment] = Int.(floor.([center_x/indicies, center_y/indicies]))
+   end
+end
 
-
-
-
-
-seg = felzenszwalb(img, 10, 100);
-weight_fn(i,j) = euclidean(segment_pixel_count(seg,i), segment_pixel_count(seg,j));
-G, vert_map = region_adjacency_graph(seg, weight_fn);
-
-g = graphfamous("karate")
-gplot(g)
+heatmap(labels_map(segments))
