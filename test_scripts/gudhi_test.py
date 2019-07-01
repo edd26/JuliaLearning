0a2d1d55279f618b81e8d11496f196a61ec00076
Emil Dmitruk <ed18abc@herts.ac.uk>
2019-07-01 13:36:34 +0100

    Added code for full matrix

--- test_scripts/gudhi_test.py
+++ test_scripts/gudhi_test.py
@@ -40,6 +40,7 @@ for filtered_value in simplex_tree.get_filtration():
 # Distance matrix from a file
 import gudhi
 import csv
+import copy
 
 matrix = [[],
           [6.0827625303],
@@ -57,12 +58,15 @@ print("Matrix element value: {}".format((matrix[0])))
 
 # ---
 # Load geometric matrix containing distances and remove diagonal and the values
-# above it
+# above it.
+# Removing values is notnecessary, as the Rips complex creator supports full
+# distance matrix.
 
 with open('distances.csv', 'r') as f:
     reader = csv.reader(f)
     your_list = list(reader)
 
+distances = copy.deepcopy(your_list)
 counter = 0
 size = len(your_list)
 
@@ -75,14 +79,30 @@ for element in your_list:
     your_list[counter] = list(map(float, element))
     counter +=1
 
-
+counter = 0
+for element in distances:
+    distances[counter] = list(map(float, element))
+    counter +=1
 
 # ---
+rips_complex = gudhi.RipsComplex(distance_matrix=distances)
+rips_complex2 = gudhi.RipsComplex(distance_matrix=your_list,
+                                 max_edge_length=20.0)
 
-rips_complex = gudhi.RipsComplex(distance_matrix=your_list,
-                                 max_edge_length=12.0)
+simplex_tree = rips_complex.create_simplex_tree(max_dimension=3)
+simplex_tree2 = rips_complex2.create_simplex_tree(max_dimension=3)
 
-simplex_tree = rips_complex.create_simplex_tree(max_dimension=1)
+
+simplex_tree.persistence(min_persistence=-1)
+simplex_tree.betti_numbers()
+
+simplex_tree2.persistence()
+simplex_tree2.betti_numbers()
+
+
+
+
+
 result_str = 'Rips complex is of dimension ' + repr(simplex_tree.dimension()) + ' - ' + \
     repr(simplex_tree.num_simplices()) + ' simplices - ' + \
     repr(simplex_tree.num_vertices()) + ' vertices.'
@@ -90,3 +110,8 @@ print(result_str)
 fmt = '%s -> %.2f'
 for filtered_value in simplex_tree.get_filtration():
     print(fmt % tuple(filtered_value))
+
+
+# Maybe it would be worth to try implement ordering complex in this library
+
+# From here, we can use simplex tree to obtain betti numbers of the complex
