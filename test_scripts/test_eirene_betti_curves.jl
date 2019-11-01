3ad9a82ae159511b41c7d10aeb630b941751f81d
edd26 <ed18abc@herts.ac.uk>
2019-11-01 11:22:47 +0000

    Variables values changed

--- test_scripts/test_eirene_betti_curves.jl
+++ test_scripts/test_eirene_betti_curves.jl
@@ -11,19 +11,20 @@ using Plots
 
 ## generate random matrix for comparison
 random_matrix = generate_random_matrix(40)
+random_matrix = generate_geometric_matrix(40)
 
 ran_betti_num, edgeDensities, persistenceIntervals, unboundedIntervals =
          compute_clique_topology(random_matrix)
 R = eirene(random_matrix,maxdim=3,model="vr")
 
-p1 = plot_betti_numbers(ran_betti_num, edgeDensities, "Random  matrix clique top");
+p1 = plot_betti_numbers(ran_betti_num, edgeDensities, "Geometric  matrix clique top");
 # %%
 betti_0 = betticurve(R, dim=0)
 betti_1 = betticurve(R, dim=1)
 betti_2 = betticurve(R, dim=2)
 betti_3 = betticurve(R, dim=3)
 
-p2 = plot(betti_0[:,1], betti_0[:,1], label="beta_0", title="Random matrix Eirene"); #, ylims = (0,maxy)
+p2 = plot(betti_0[:,1], betti_0[:,1], label="beta_0", title="Geometric matrix Eirene"); #, ylims = (0,maxy)
 plot!(betti_1[:,1], betti_1[:,2], label="beta_1");
 plot!(betti_2[:,1], betti_2[:,2], label="beta_2");
 plot!(betti_3[:,1], betti_3[:,2], label="beta_3");
@@ -31,7 +32,7 @@ plot!(betti_3[:,1], betti_3[:,2], label="beta_3");
 plot_ref = plot(p1, p2, layout = (2,1))
 
 cd("/home/ed19aaf/Programming/Julia/JuliaLearning")
-savefig(plot_ref, "results/comparison_random_matrix.png")
+savefig(plot_ref, "results/comparison_geometric_matrix.png")
 
 
 
